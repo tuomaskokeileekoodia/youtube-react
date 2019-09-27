fcdfaf6443c92d06b332ad1a1f29624495aee541
tuomaskokeileekoodia <tuomaslahti89@gmail.com>
2019-09-27 15:34:48 +0300

    Tykkää-nappi lisätty

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    count: 0
+  }
+
+
+  //Tykkää-napin lähde: https://medium.com/@elle.westervelt/create-a-basic-like-button-in-react-5274a6991385
+  incrementMe = () => {
+    let newCount = this.state.count +1
+    this.setState({
+      count: newCount
+    })
+  }
+
+  render() {
+    return (
+        <div>
+          <p>Hello...</p>
+          <button onClick={this.incrementMe}>❤ Likes: {this.state.count} </button>
+        </div>
+    );
+  }
 }
 
 export default App;
